0df303c47a1942f8d48a97dbae0151c2ddb741bb
Darken-027 <torut1638@gmail.com>
2024-10-05 22:36:41 -0500

    affected file

--- Curso-JavaScript/mi-primer-proyecto-de-node/index.js
+++ Curso-JavaScript/mi-primer-proyecto-de-node/index.js
@@ -5,7 +5,7 @@ console.log("Esta es la puerta de entrada al proyecto")
         Esta es la parte intermedia de nuestro proyecto
         En esta parte realizamos los chequeos de nuestro proyecto
         Por ultimo vamos a la linea de saludo
-        
+
         todo esto tambien esta comentado
     */
 // Este va a ser el mensaje que se envia para despedir
