d0ad6034bb8a1432bfad35408f5b75e2d91f04bf
Darken-027 <torut1638@gmail.com>
2024-10-08 18:50:14 -0500

    restauracion

---
+++ Curso-JavaScript/funcionesMatematicas.js
@@ -0,0 +1,26 @@
+// Funciones matematicas
+
+//Redondea a seguiente entero en decimal -5
+var precio = Math.round(399.53);
+document.write('precio redondeado: ', precio);
+document.write('<br>');
+
+//Redondea sin importar decimal asia arriba
+var precio = Math.ceil(299.9);
+document.write('precio redondeado: ', precio);
+document.write('<br>');
+
+//Redondea sin importar decimal asia abajo
+var precio = Math.floor(540.9);
+document.write('precio redondeado: ', precio);
+document.write('<br>');
+
+
+var seno = Math.sin(45);
+document.write('seno de 45: ', seno );
+
+
+
+
+
+
---
+++ Curso-JavaScript/tipos-primitivos.js
@@ -0,0 +1,28 @@
+// Tipos primitivos
+
+// number
+4;
+0;
+
+// string
+"Hola mundo";
+'Hola mundo';
+`hola mundo`;
+
+// booleanos
+true;
+false;
+
+// nulo y undefined
+null;
+undefined;
+
+// null, undefined, false, 0 ==> todas son falsy
+console.log(null === undefined)
+if(tre) {
+    // si cumple
+    console.log("cumple");
+} else {
+    // si no cumple
+    console.log("no cumple");
+}
